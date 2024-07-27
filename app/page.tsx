9337c225ee9781bbd560e7e3eeba51a4729f0090
adiputha <adithyaperera456@gmail.com>
2024-07-27 13:37:45 +0530

    forms

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,45 @@
-
+import { Button } from "@/components/ui/button"
+import Image from "next/image";
+import  PatientForm  from "@/components/forms/PatientForm"
+import Link from "next/link";
 
 export default function Home() {
  return(
-  <div>
-   <h1 className="text-3xl underline">Home</h1>
+  <div className="flex h-screen max-h-screen">
+
+    {/*todo: OTP verficatiopn passkey */}
+
+   <section className="remove-scrollbar container my-auto">
+    <div className="sub-container max-w-[496x]">
+        <Image
+            src="/assets/icons/logo-full.svg"
+            alt="patient"
+            width={1000}
+            height={1000}
+            className="mb-12 h-10 w-fit"
+            />
+
+            <PatientForm/>
+            <div className="text-14-regular mt-20 flex justify-between">
+                <p className="justify-items-end text-dark-600 xl:text-left">
+                © 2024 CareConnect
+                </p>
+                <Link href="/?admin=true" className="text-green-500">
+                admin
+                </Link>
+            
+            </div>
+    </div>
+
+   </section>
+
+   <Image
+    src="/assets/images/onboarding-img.png"
+    height={1000}
+    width={1000}
+    alt="patient"
+    className="side-img max-w-[50%]"
+   />
   </div>
  )
 }
